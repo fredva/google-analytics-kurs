3de670f0dc4fa18b8250987f3145fab4b26fd18f
Fredrik Valdmanis <fredrik@valdmanis.com>
2016-10-11 19:45:25 +0200

    Fiks trackingbug i handlekurv

--- js/handlekurv.js
+++ js/handlekurv.js
@@ -13,6 +13,15 @@ ga('ec:setAction', 'add');
 
 ga('send', 'pageview');
 
+ga('ec:addProduct', {
+    'name': 'TV',
+    'category': 'Lyd og bilde',
+    'brand': 'Samsung',
+    'variant': '50 tommer',
+    'price': '9999',
+    'quantity': 2
+});
+
 ga('ec:setAction','checkout', {
     'step': 1
 });
